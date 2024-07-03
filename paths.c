42b4521ae6603c0b8442107eb0babd05ca8e480c
Chris <chris_aitch@fastmail.co.uk>
2024-07-03 18:28:25 +0200

    Redirection: functions for output
    * append
    * redirect
    * (Possibility to combine them)
    * Also find_flow_control to scan cmd args and return position of
      any relevant chars (< etc)
      ...that will need to be replaced later.

--- paths.c
+++ paths.c
@@ -19,6 +19,7 @@
 // -- does path + cmd = an executable?
 // -- if YES we have our command: keep that and discard the rest.
 // NOTE The return value of getenv("PATH") does not need to be freed
+// TODO Change the name of this, command is too ambiguous. Executable?
 char	*find_command(char *cmd)
 {
 	char	**pathparts;
@@ -47,6 +48,79 @@ char	*find_command(char *cmd)
 	return (goodpath);
 }
 
+// Look through a parsed set of commands and see if it contains
+// flow control parameters: | > < >> or <<
+// They would be separate from commands either side.
+// FIXME This is PoC only: expand to other flow things
+// - [ ] >>
+// - [ ] <
+// - [ ] <<
+// - [ ] |
+// TODO If not found, return -1.
+int	find_flow_control(t_command *cmdset)
+{
+	int	i;
+
+	i = 0;
+	while (i < cmdset->argc)
+	{
+		if (ft_strncmp(cmdset->argv[i], ">>", 2 == 0))
+			break ;
+		else if (ft_strncmp(cmdset->argv[i], "<<", 2 == 0))
+			break ;
+		else if (ft_strncmp(cmdset->argv[i], ">", 1 == 0))
+			break ;
+		else if (ft_strncmp(cmdset->argv[i], "<", 1 == 0))
+			break ;
+		i++;
+	}
+	return (i);
+}
+
+// Find the path part (after the redirect >>)
+// Open a file of that name for writing.
+// Set the output to that file.
+// run the command in a child process
+// close file when done.
+// FIXME This will not work without mangling the argvs
+// NOTE The main (only?) difference from redirect_output is the O_APPEND flag
+// TODO This could be combined with redirect: just set file options differently.
+void	run_in_child_append_output(t_command *cmd, char **envp)
+{
+	int	out_file;
+	int	redirect_posn;
+
+	redirect_posn = find_flow_control(cmd);
+	out_file = open(cmd->argv[redirect_posn + 1], O_WRONLY | O_CREAT | O_APPEND, 0777);
+	if (out_file == -1)
+		perror("Could not open output file");
+	dup2(out_file, STDOUT_FILENO);
+	close (out_file);
+	run_in_child(cmd, envp);	// FIXME This will not work without mangling argvs!
+}
+
+// Find the path part (after the redirect >)
+// Open a file of that name for writing.
+// Set the output to that file.
+// run the command in a child process
+// close file when done.
+// FIXME This will not work without mangling the argvs
+// TODO Think about how to find the needed parts
+// TODO Do we need to reset STDOUT afterwards, or is this going to be process-limited?
+void	run_in_child_redirect_output(t_command *cmd, char **envp)
+{
+	int	out_file;
+	int	redirect_posn;
+
+	redirect_posn = find_flow_control(cmd);
+	out_file = open(cmd->argv[redirect_posn + 1], O_WRONLY | O_CREAT | O_TRUNC, 0777);
+	if (out_file == -1)
+		perror("Could not open output file");
+	dup2(out_file, STDOUT_FILENO);
+	close (out_file);
+	run_in_child(cmd, envp);	// FIXME This will not work without mangling argvs!
+}
+
 // Make a child process to execute the command, putting the output in a pipe
 // - fork
 // - run command
@@ -54,7 +128,7 @@ char	*find_command(char *cmd)
 // NOTE child == 0 means we are in the child process!
 // TODO We can make this work with & / background
 // TODO How would this work with different input / output?
-//
+// TODO Can I use the output from this to measure state?
 void	run_in_child_with_pipe(t_command *cmd, char **envp)
 {
 	pid_t	child;
@@ -83,6 +157,7 @@ void	run_in_child_with_pipe(t_command *cmd, char **envp)
 
 // Simplest command runner.
 // Launches one command in a child process and waits for it to complete.
+// NOTE This is the one we use for simple commands.
 void	run_in_child(t_command *cmd, char **envp)
 {
 	pid_t	child;
