bebee6528a50066b375bfc16376ec8fc8580cd05
Brillande <jelland90@gmail.com>
2024-07-17 17:03:08 +0000

    fixing export

--- builtins.c
+++ builtins.c
@@ -37,7 +37,8 @@ void	ms_exit(t_command *cmd)
 // If the variable does not already exist, add it.
 // NOTE I think we have to distinguish between the variable's NAME and VALUE
 // FIXME This has variables defined in-line at time of use.
-void ms_unset_export(char *unset_var, char **envp) {
+void ms_unset_export(char *unset_var, char **envp) 
+{
     int var_index = 0; 
     var_index = find_env_var(envp, unset_var);
 
@@ -46,11 +47,11 @@ void ms_unset_export(char *unset_var, char **envp) {
         // Liberar la memoria de la variable de entorno
 
         // Eliminar la variable de entorno del arreglo envp
-        int i = var_index;
-        while (envp[i] != NULL) 
+
+        while (envp[var_index] != NULL) 
         {
-            envp[i] = envp[i + 1];
-            i++;
+            envp[var_index] = envp[var_index + 1];
+            var_index++;
         }
     } 
     else 
@@ -72,68 +73,83 @@ void ms_export(t_command *cmd, char **envp)
     int len_unset;
     char *unset_var;
     len_unset = 0;
+    int len1 = 0;
+    char *var2;
     int len = 0;
     int j = 0;
+    int h = 1;
     if (cmd->argc < 2) 
     {
         printf("export: missing argument\n");
         return;
     }
-    var = cmd->argv[1];
-    while (cmd->argv[1][len_unset] != '=')
-            len_unset++;
-    unset_var = malloc(sizeof(char) * len_unset + 1);
-    while (j <= len_unset - 1)
+    while(cmd->argv[h] != NULL)
     {
-        unset_var[j] = var[j];
-        j++;
+        if(cmd->argv[h][0] == '=')
+        {
+            var2 = cmd->argv[h];
+            len1= ft_strlen(var2);
+            var2[len1]= '\0';
+            printf("export : %s not a valid identifier",var2);
+            h++;
+        }
+        var = cmd->argv[h];
+        while (cmd->argv[1][len_unset] != '=' && cmd->argv[1][len_unset] != '\0')
+            len_unset++;
+        unset_var = malloc(sizeof(char) * len_unset + 1);
+        while (j <= len_unset - 1)
+        {
+            unset_var[j] = var[j];
+            j++;
+        }
+        unset_var[j] = '\0';
+            printf("\n\n\n-----%s------\n\n\n",unset_var);
+            ms_unset_export(unset_var,envp);
+            size_t env_len = 0;
+            while (envp[env_len] != NULL) 
+            {
+                env_len++;
+            }
+
+            char **new_envp = malloc(sizeof(char *) * (env_len + 2));
+            if (new_envp == NULL) 
+            {
+                perror("malloc");
+                return;
+            }
+
+            size_t i = 0;
+            while (i < env_len) 
+            {
+                new_envp[i] = envp[i];
+                i++;
+            }
+
+            new_var = ft_strdup(var);
+            len = ft_strlen(new_var);
+            if(new_var[len - 1] == '=')
+                new_var = ft_strjoin(new_var,"\"\"");
+            if (new_var == NULL) 
+            {
+                perror("ft_strdup");
+                free(new_envp);
+                return;
+            }
+            new_envp[env_len] = new_var;
+            new_envp[env_len + 1] = NULL;
+            i = 0;
+            while (i < env_len + 1) 
+            {
+                envp[i] = new_envp[i];
+                i++;
+            }
+            envp[env_len + 1] = NULL;
+
+            free(new_envp);
+            h++;
     }
     
-        printf("\n\n\n-----%s------\n\n\n",unset_var);
-        ms_unset_export(unset_var,envp);
-        size_t env_len = 0;
-        while (envp[env_len] != NULL) 
-        {
-            env_len++;
-        }
-
-        char **new_envp = malloc(sizeof(char *) * (env_len + 2));
-        if (new_envp == NULL) 
-        {
-            perror("malloc");
-            return;
-        }
-
-        size_t i = 0;
-        while (i < env_len) 
-        {
-            new_envp[i] = envp[i];
-            i++;
-        }
-
-        new_var = ft_strdup(var);
-        len = ft_strlen(new_var);
-        if(new_var[len - 1] == '=')
-        {
-            new_var = ft_strjoin(new_var,"\"\"");
-        }
-        if (new_var == NULL) 
-        {
-            perror("ft_strdup");
-            free(new_envp);
-            return;
-        }
-        new_envp[env_len] = new_var;
-        new_envp[env_len + 1] = NULL;
-        i = 0;
-        while (i < env_len + 1) 
-        {
-            envp[i] = new_envp[i];
-            i++;
-        }
-        envp[env_len + 1] = NULL;
-
-        free(new_envp);
+        
 }
 
 
@@ -141,13 +157,19 @@ void ms_export(t_command *cmd, char **envp)
 // If not present, take no action.
 // FIXME This causes an endless loop, I guess in the while(envp[i])
 // FIXME Also cause a segfault!
-void ms_unset(t_command *cmd, char **envp) {
+void ms_unset(t_command *cmd, char **envp) 
+{
+    int i;
+
+    i = 0;
     if (cmd->argc < 2) 
     {
         printf("unset: missing argument\n");
         return;
     }
-    char *var = cmd->argv[1];
+    i = ft_strlen(cmd->argv[1]);
+    char *var = malloc(sizeof (char *) * i);
+    var = cmd->argv[1];
     int var_index = find_env_var(envp, var);
 
     if (var_index >= 0) 
--- helpers.c
+++ helpers.c
@@ -18,9 +18,8 @@ int	find_env_var(char **envp, const char *var)
 	{
 		if (ft_strncmp(envp[i], var, len) == 0 && envp[i][len] == '=')
 			return (i);
-		if(ft_strncmp(envp[i], var, len) == 0 && envp[i][len] != '=')
+		else if(ft_strncmp(envp[i], var, len) == 0 && envp[i][len] != '=')
 			return(i);
-		printf("%s",var);
 		i++;
 	}
 	return (-1);
