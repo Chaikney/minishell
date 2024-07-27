2e5af0ff75afb77513a25032da87bc94876f660c
Chris <chris_aitch@fastmail.co.uk>
2024-07-27 13:48:20 +0200

    Signals: FIX behaviour of CTRL-C / SIGINT
    Per SO answer: https://stackoverflow.com/a/41917863

--- signals.c
+++ signals.c
@@ -13,19 +13,26 @@ void	handle_sigquit(int sig)
 }
 
 // catch CTRL-c / SIGINT
-// FIXME This should return to the normal prompt / readline call
+// FIXED This should return to the normal prompt / readline call
 // ...I think this leads to duplicated prompt message.
 // "CTRL-C displays a new prompt on a new line."
+// Works like: https://stackoverflow.com/a/41917863
+// - Move to a new line
+// - regenerate prompt
+// - clear previous text
 void	handle_sigint(int sig)
 {
 	if (sig)
 	{
-		printf("\nwhat should i do? >");
+		printf("\n");
+		rl_on_new_line();
+		rl_replace_line("", 0);
+		rl_redisplay();
 	}
 }
 
 // Setup the functions to handle each signal we have to manage.
-// TODO Implement signals handling SIGINT - needs fixed
+// DONE Implement signals handling SIGINT - needs fixed
 // TODO Implement signals handling CTRL-D
 // TODO Implement signals handling CTRL-\ uncontrolled quit
 int	setup_signals(void)
