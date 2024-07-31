52e8be0ba28c1081d4146ff1606e801a50c217a9
Chris Haikney <chaikney@c4r5s1.42urduliz.com>
2024-07-31 14:17:46 +0200

    ECHO: init var to quieten compiler
    Should not ever be needed.

--- ms_echo.c
+++ ms_echo.c
@@ -14,14 +14,11 @@
 
 // Minishell implementation of the echo command.
 // Supports the -n option (NO newline after output)
-// FIXED Too many lines, we can consolidate this.
-// DONE Double check the echo / echon split and i values.
-// FIXED These names were the wrong way round! ECHON should not have \n
-// NOTE The output is correct but this setup is confusing.
 void	ms_echo(t_command *cmd)
 {
 	int	i;
 
+	i = 0;
 	if (cmd->builtin == ECHO)
 		i = 1;
 	else if (cmd->builtin == ECHON)
